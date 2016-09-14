216dd6052439b1a5ffc4bfdf6920df2a0b9e4f3a
starriv.993@gmail.com <starriv.993@gmail.com>
2016-09-14 09:42:49 +0800

    ADD：add a some annotation

--- src/components/hello/hello.jsx
+++ src/components/hello/hello.jsx
@@ -6,27 +6,40 @@ import './hello.less';
 class Hello extends Component {
 
     static propTypes = {
+        // action
         actions: PropTypes.object.isRequired,
+
+        //初始的state
         addmessage: PropTypes.object
     }
 
-    onClick() {
-        this.props.actions.addMessage(this.props.addmessage);
-    }
-
     render() {
+        const {actions, addmessage} = this.props;
         return (
-            <p className="hello" onClick={() => this.onClick()}>Hello React</p>
+            <p className="hello" onClick={() => actions.addMessage(addmessage)}>Hello React</p>
         );
     }
 }
 
-export default connect((store) => {
-    return {addmessage: store.addmessage};
-}, (dispatch) => {
+// 哪些 Redux 全局的 state 是我们组件想要通过 props 获取的？
+function mapStateToProps(state) {
+    return {addmessage: state.addmessage};
+}
+
+// 哪些 action 创建函数是我们想要通过 props 获取的？
+function mapDispatchToProps(dispatch) {
     return {
         actions: bindActionCreators({
             addMessage
         }, dispatch)
     };
-})(Hello);
+}
+/**
+* 通过connect 链接 Redux store. 统一分发 state 和 actions.
+* 实际这里应该传入一个根组件. 通过根组件分发
+**/
+// ES7 Decorators简化版
+// @connect(mapStateToProps, mapDispatchToProps )
+// export default class hello{}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hello);
